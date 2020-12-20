db2d36b85573097b93a5bb7938371e40df31bc25
Denny Feng <dennyfeng@MacBook-Pro.fios-router.home>
2020-12-20 01:02:46 -0500

    Add mikroORM, post.ts, constants.ts...

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log("Hello, Denny");
+import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "./constants";
+import { Post } from "./entities/Post";
+
+const main = async () => {
+    const orm = await MikroORM.init({
+        entities: [Post],
+        dbName: 'lireddit',
+        type: 'postgresql',
+        debug: !__prod__,
+    });
+
+    const post = orm.em.create(Post, {title: 'My First Post'});
+}
+
+main();
